8da31e701e93468d3cb30c48abad1374e87678e0
TrangDC <trangdinhcongtmqt@gmail.com>
2026-07-02 21:28:07 +0700

    Initial CKEditor custom build

---
+++ config.js
@@ -0,0 +1,5 @@
+﻿/*
+ * Cấu hình CKEditor được quản lý hoàn toàn từ CKEditorWrapper.tsx.
+ * File này được giữ trống để tránh conflict với instance config.
+ */
+CKEDITOR.editorConfig = function () {};
